c967ca4997b0c649c553d0912f499dc2fabdc7c7
rafaelvgl <rafaelvgl@gmail.com>
2026-05-23 16:34:04 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+const BONITA_CONFIG = {
+  saipos: {
+    baseUrl: "https://api.saipos.com",
+    token: "SEU_TOKEN_AQUI",
+    storeId: "SEU_STORE_ID"
+  },
+  brand: {
+    name: "Bonità Pizzaria",
+    primaryColor: "#D6453D",
+    secondaryColor: "#024D34"
+  }
+};
